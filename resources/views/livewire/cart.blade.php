e899e7b26a361bd1cfba0d10c30712da663c4541
stonear <stonear@users.noreply.github.com>
2020-09-25 23:11:18 +0700

    V 0.6

--- resources/views/livewire/cart.blade.php
+++ resources/views/livewire/cart.blade.php
@@ -73,7 +73,7 @@
                             <td class="py-2 text-gray-700 text-right">@rupiah($c['total'])</td>
                             <td class="py-2 w-1">
                                 <button class="text-gray-800 font-bold flex item-center" wire:click="removeItem({{ $c['id'] }}, {{ $c['price']['id'] }})">
-                                    <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>
+                                    <svg class="w-5 h-5 text-orange-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>
                                 </button>
                             </td>
                         </tr>
@@ -87,7 +87,8 @@
                     </tfoot>
                 </table>
                 @else
-                <div class="w-full text-center">
+                <div class="w-full text-center text-orange-500 pt-20">
+                    <svg class="w-full text-center w-24 h-24 pb-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
                     Keranjang Kosong
                 </div>
                 @endif
